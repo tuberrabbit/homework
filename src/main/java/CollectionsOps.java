bf02dd09e8f2dda962866f33cc77da737dbb7145
cqupt-gsy <gaosy0106@gmail.com>
2015-11-21 11:53:08 +0800

    "first version"

--- src/main/java/CollectionsOps.java
+++ src/main/java/CollectionsOps.java
@@ -1,16 +1,41 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionsOps {
 
     public static List<Integer> odd(List<Integer> numbers) {
-        return null;
+        List<Integer> results = new ArrayList<Integer>();
+
+        for (Integer item: numbers) {
+            if (item % 2 != 0){
+                results.add(item);
+            }
+        }
+        return results;
     }
 
     public static List<Integer> doubleEvens(List<Integer> numbers) {
-        return null;
+        List<Integer> results = new ArrayList<Integer>();
+        for (Integer item: numbers) {
+            if (item % 2 == 0){
+                results.add(item*2);
+            }
+            else {
+                results.add(item);
+            }
+        }
+        return results;
     }
 
     public static String tryFind(Integer toFind, List<Integer> numbers) {
-        return null;
+        String result = "Not Found";
+        for (Integer item: numbers) {
+            if (item == toFind){
+                result = "Hello, " + item.toString();
+                break;
+            }
+        }
+        return result;
+
     }
 }
