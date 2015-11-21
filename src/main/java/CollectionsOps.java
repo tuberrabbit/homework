4c04b0fa0d2a17252996c633eee27610611e5e75
tuber <btu@thoughtworks.com>
2015-11-21 11:07:07 +0800

    [tuber] complete test

--- src/main/java/CollectionsOps.java
+++ src/main/java/CollectionsOps.java
@@ -1,16 +1,25 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CollectionsOps {
 
     public static List<Integer> odd(List<Integer> numbers) {
-        return null;
+        return numbers.stream()
+                .filter(number -> number % 2 != 0)
+                .collect(Collectors.toList());
     }
 
     public static List<Integer> doubleEvens(List<Integer> numbers) {
-        return null;
+        return numbers.stream()
+                .map(number -> number % 2 == 0 ? number * 2 : number)
+                .collect(Collectors.toList());
     }
 
     public static String tryFind(Integer toFind, List<Integer> numbers) {
-        return null;
+        if (numbers.stream()
+                .anyMatch(number -> number.equals(toFind))) {
+            return "Hello, " + toFind;
+        }
+        return "Not Found";
     }
 }
